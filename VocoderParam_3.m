94cb5b9174a140e60956d8cef1c20eaca9e5eeb7
frabia <bianco83d19@yahoo.it>
2017-02-22 10:59:27 +0100

    Update VocoderParam_3.m

--- VocoderParam_3.m
+++ VocoderParam_3.m
@@ -5,14 +5,14 @@ binsize=fc/winsize;
 
 
 A= 0;
-f=binsize*200;
+f=binsize*1;
 dur=0.5;
 t=[0:sinc:dur];
 
-ampEnvSig=0.5*sin(20*2*pi*t);
+ampEnvSig=0.5+(0.5*cos(2*pi*0.5*t));
 ampEnv=A.+ampEnvSig; %Modulante per l'ampiezza
 
-phiEnvSig=40*sin(0.05*2*pi*t);
+phiEnvSig=5*sin(2*pi*5*t);
 phiEnv=f.+phiEnvSig; %Modulante per la fase
 
 w=f*2*pi;
@@ -35,7 +35,7 @@ zbDer=zb(2:size(angle(zb),2)-1)-(zb(1:size(angle(zb),2)-2));%DEVIAZIONE DI FREQU
 for n=1:winsize
     c1=exp(-i*(2*pi*F(n)*t)); 
     output1=sum(x.*c1);
-    mag1(n)=2*abs(output1)/winsize;
+    mag1(n)=20*log10(2*(abs(output1)/winsize));
     fasitot1(n)=arg(output1);
     fasi1(n)=0;
     if mag1(n) > 0.05
@@ -45,7 +45,7 @@ end
 for k=1:winsize
     c=exp(-i*(2*pi*F(k)*t)); 
     output=sum(z.*c);
-    mag(k)=2*abs(output)/winsize;
+    mag(k)=20*log10(2*(abs(output)/winsize));
     fasitot(k)=arg(output);
     fasi(k)=0;
     if mag(k) > 0.05
@@ -56,12 +56,12 @@ end
 
 figure(1)
 plot (F, fasi)
-axis ([-fc,fc, -pi, pi])
+axis ([-(f*10),f*10, -pi, pi])
 title("FASE di z")
 
 figure(2)
-plot (F, 20*log10(mag))
-axis ([-fc,fc, -70, 0])
+plot (F, mag)
+axis([-(f*10),f*10, -100, 20])
 title("MAGNITUDINE di z")
 
 figure(3)
@@ -71,7 +71,7 @@ title("Z=SEGNALE ANALITICO")
 
 figure(4)
 plot (t,zAt)
-axis ([0,0.5, -1, 1])
+axis ([0,1, -1, 1])
 title("INV AMP di z")
 
 tder=[1:size(t,2)-2];
@@ -82,10 +82,10 @@ title("INV FREQ di z")
 
 figure(6)
 plot (F, fasi1)
-axis ([-fc,fc, -pi, pi])
+axis ([-(f*10),f*10, -pi, pi])
 title("FASE DI x")
 
 figure(7)
-plot (F, 20*log10(mag1))
-axis ([-fc,fc, -70, 0])
+plot (F, mag1)
+axis ([-(f*10),f*10, -100, 20])
 title("MAGNITUDINE DI x")
